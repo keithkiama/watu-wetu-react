5bbb755c512c9efa3f255dab476b07459d0db523
Keith Kiama Maina <keithkiama2003@gmail.com>
2023-01-19 11:17:13 +0300

    Fixed Syntax errors on community.js

--- src/components/community.jsx
+++ src/components/community.jsx
@@ -1,26 +1,12 @@
 import Member from "./member";
-import { useState, useEffect } from "react";
+import useData from "../hooks/use-data";
 
-const Community = ({ limit, endpoint }) => {
-    const [members, setMembers] = useState([]);
-
-    useEffect(() => { 
-        (async (endpoint) => {
-        try {
-            const resp = await fetch(`http://localhost:4000/${endpoint}?${limit ? `_limit=${limit}` : ''}`);
-            const data = await resp.json();
-          
-            setMembers(data);
-        } catch (error) {
-            setMembers([]);
-        }
-    })()
-    },
-        [endpoint, limit]);
+const Community = ({ endpoint }) => {
+    const data = useData(endpoint);
 
     return <div className="row">
         {
-            members.map((member, index) => {
+            (data?.record?.africans || []).map((member, index) => {
                 return <div key={index} className="col-12 col-md-4 col-xl-3 mt-4 mb-2">
                     <Member {...member} />
                 </div>;
